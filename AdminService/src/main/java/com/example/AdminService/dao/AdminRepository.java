e7b4afc076a2608b8c266cd3b246d3778e6ad2e8
Shib02 <82205932+Shib02@users.noreply.github.com>
2021-07-01 18:04:45 +0530

    Delete AdminService directory

--- AdminService/src/main/java/com/example/AdminService/dao/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AdminService.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.AdminService.entity.TrainDetails;
-
-public interface AdminRepository extends MongoRepository<TrainDetails, String> {
-
-}
